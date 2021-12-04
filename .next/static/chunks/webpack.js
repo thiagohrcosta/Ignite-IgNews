ba2a9359619febb16174ea613b0eb2cad522b934
Thiago Costa <thiagohrcosta86@gmail.com>
2021-12-04 18:04:57 -0300

    Finishing first part

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ce19785f29b8965"; }
+/******/ 		__webpack_require__.h = function() { return "69379abea1e14e21"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e2eae79622534631.hot-update.js
+++ .next/static/webpack/webpack.e2eae79622534631.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a350df222cd6c280"; }
+/******/ 	__webpack_require__.h = function() { return "bff4b45ac6786724"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
