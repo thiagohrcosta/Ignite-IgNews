4e17906d9e189fae58f16c752b267169eda8fb8e
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-04-19 18:03:36 -0300

    Trying to fix github bug

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,31 +1,31 @@
-import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut,useSession } from 'next-auth/react'
 
-import styles from './styles.module.scss';
+export function SignInButton(){
+  const { data, status } = useSession()
 
-export function SignInButton() {
-  const { data: session } = useSession()
-  console.log(session)
+  console.log(data);
 
-  return session ? (
+  return status === "authenticated" ? (
     <button
       type="button"
       className={styles.signInButton}
       onClick={() => signOut()}
     >
-      <FaGithub color="#04d361" />
-      {session.user.name}
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FaGithub color="#04d361"/>
+      {data.user.name}
+      <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => signIn('github')}
+      onClick={() => signIn('GithubProvider')}
     >
-      <FaGithub color="#eba417" />
+      <FaGithub color="#eba417"/>
       Sign in with Github
     </button>
-  );    
+  );
 }
